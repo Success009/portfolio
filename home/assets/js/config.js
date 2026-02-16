944a556e9368e4036648fb66c94446da4bd132c1
Success009 <119729030+Success009@users.noreply.github.com>
2026-02-16 21:47:13 +0545

    Delete home/assets directory

--- home/assets/js/config.js
+++
@@ -1,7 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyA6sANvYoAkXHYG8MjbZl6Pyq23CNdBuzA",
-    databaseURL: "https://community-canvas-255fa-default-rtdb.firebaseio.com",
-    projectId: "community-canvas-255fa",
-};
-
-export default firebaseConfig;
